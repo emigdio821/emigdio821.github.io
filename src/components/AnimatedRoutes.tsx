9573791a5f1430f70dd44b4642ed29699c2e1788
Emigdio Torres <emigdio821@gmail.com>
2022-05-25 12:49:47 -0500

    Removed extra import

--- src/components/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes.tsx
@@ -1,7 +1,6 @@
 import { AnimatePresence } from "framer-motion";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { routes } from "../routes/routes";
-import { Footer } from "./Footer";
 import { MotionContainer } from "./MotionContainer";
 
 if (typeof window !== "undefined") {
