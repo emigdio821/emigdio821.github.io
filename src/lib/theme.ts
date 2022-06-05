56e7372e013841f9b3f154be39f3a6fe987f9a32
Emigdio Torres <emigdio821@gmail.com>
2022-06-05 14:38:41 -0500

    Converted theme.js file to ts

---
+++ src/lib/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from "@chakra-ui/react";
+import { mode, StyleFunctionProps } from "@chakra-ui/theme-tools";
+import { Dict } from "@chakra-ui/utils";
+import TextColorModeVal from "utils/TextColorModeVal";
+
+const styles = {
+  global: (props: StyleFunctionProps | Dict<any>) => ({
+    body: {
+      color: TextColorModeVal,
+      bg: mode("inherit", "#333")(props),
+    },
+  }),
+};
+
+const shadows = {
+  outline: "0 0 0 3px #B2ABCC",
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config, styles, shadows });
+export default theme;
