af7b9eb543ad1f8e4b5be9a89755e459322757fc
Emigdio Torres <emigdio821@gmail.com>
2022-06-17 20:41:46 -0500

    Added vite config

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  plugins: [react(), svgr(), tsconfigPaths()],
+});
